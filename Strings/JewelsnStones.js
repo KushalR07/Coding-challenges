ab34cb90376e46f6441eb92ab51e545d60121bd7
Kushal R <Kushal.r@kiksarvr.com>
2026-02-28 21:40:02 +0530

    Day-3

---
+++ Strings/JewelsnStones.js
@@ -0,0 +1,32 @@
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+// Example 1:
+
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    let count=0;
+    let seen = new Set();
+
+    for(let i=0;i<jewels.length;i++){
+        seen.add(jewels[i]);
+    }
+
+    for(let i=0;i<stones.length;i++){
+        if(seen.has(stones[i])){
+            count++;
+        }
+    }
+    return count;
+
+};
