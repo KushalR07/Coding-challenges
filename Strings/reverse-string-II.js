191462e9a1280ea1b40f123d9028a04db01609f7
Kushal R <Kushal.r@kiksarvr.com>
2026-03-10 18:18:28 +0530

    day-6

---
+++ Strings/reverse-string-II.js
@@ -0,0 +1,33 @@
+// Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+
+// If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcdefg", k = 2
+// Output: "bacdfeg"
+// Example 2:
+
+// Input: s = "abcd", k = 2
+// Output: "bacd"
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+    s=s.split("");
+    for(let x=0;x<s.length;x+=2*k){
+        let n=k;
+        let mid=Math.floor(n/2);
+        for(let i=0;i<n/2;i++){
+            let temp=s[x+i];
+            s[x+i]=s[x+n-1-i];
+            s[x+n-i-1]=temp;
+        }
+    }
+    return s.join("");
+};
